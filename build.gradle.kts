838e284652f54de62955edf8a8352d9034dde070
PCBL <cksgodl1@naver.com>
2023-07-10 20:52:53 +0900

    Chore : gradle kts + BuildSrc 활용하도록 변경

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// 모든 하위 프로젝트/모듈에 공통적인 구성 옵션을 추가하는 최상위 빌드
+plugins {
+    with(Plugins) {
+        id(ANDROID_APPLICATION) version Versions.AGP apply false
+        id(ANDROID_LIBRARY) version Versions.AGP apply false
+        id(JETBRAINS_KOTLIN_ANDROID) version Versions.KOTLIN apply false
+    }
+}
