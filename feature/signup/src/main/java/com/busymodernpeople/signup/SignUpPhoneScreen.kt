7de65148e1a32572dffb7dbdace1315491246a8e
DongChyeon <kimdonghyun90766@gmail.com>
2023-08-03 16:03:19 +0900

    WIN-50 Design : 인증번호 요청 시 바텀 시트 추가

--- feature/signup/src/main/java/com/busymodernpeople/signup/SignUpPhoneScreen.kt
+++ feature/signup/src/main/java/com/busymodernpeople/signup/SignUpPhoneScreen.kt
@@ -1,23 +1,33 @@
 package com.busymodernpeople.signup
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.teamwiney.core.design.R
 import com.teamwiney.ui.components.HeightSpacer
 import com.teamwiney.ui.components.PhoneNumberVisualTransformation
 import com.teamwiney.ui.components.SignUpTopBar
@@ -25,6 +35,7 @@ import com.teamwiney.ui.components.WButton
 import com.teamwiney.ui.components.WTextField
 import com.teamwiney.ui.theme.WineyTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpPhoneScreen(
     onBack: () -> Unit,
@@ -39,6 +50,48 @@ fun SignUpPhoneScreen(
     }
     LaunchedEffect(phoneNumber) {
         errorState = !(phoneNumber.length == 11 || phoneNumber.isEmpty())
+    }
+
+    var showSheet by remember { mutableStateOf(false) }
+    if (showSheet) {
+        val modalBottomSheetState = rememberModalBottomSheetState()
+
+        ModalBottomSheet(
+            onDismissRequest = { showSheet = false },
+            sheetState = modalBottomSheetState,
+            dragHandle = { BottomSheetDefaults.DragHandle() },
+            containerColor = WineyTheme.colors.gray_950
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth().padding(start = 24.dp, end = 24.dp, bottom = 20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.mipmap.img_lock),
+                    contentDescription = null
+                )
+                HeightSpacer(height = 12.dp)
+                Text(
+                    text = "인증번호가 발송되었습니다.\n3분 안에 인증번호를 입력해주세요.",
+                    style = WineyTheme.typography.bodyB1,
+                    color = WineyTheme.colors.gray_200,
+                    textAlign = TextAlign.Center
+                )
+                HeightSpacer(height = 14.dp)
+                Text(
+                    text = "*인증번호 요청 3회 초과 시 5분 제한됩니다.",
+                    style = WineyTheme.typography.captionM2,
+                    color = WineyTheme.colors.gray_600
+                )
+                HeightSpacer(height = 26.dp)
+                WButton(
+                    text = "닫기",
+                    onClick = { onConfirm() }
+                )
+            }
+        }
+
     }
 
     Column(
@@ -78,7 +131,7 @@ fun SignUpPhoneScreen(
                 text = "확인",
                 onClick = {
                     // TODO : Add next button Event
-                    onConfirm()
+                    showSheet = true
                 },
                 enabled = phoneNumber.length == 11,
                 modifier = Modifier.padding(bottom = 20.dp)
@@ -86,4 +139,3 @@ fun SignUpPhoneScreen(
         }
     }
 }
-
