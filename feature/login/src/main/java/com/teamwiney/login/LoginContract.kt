9d26c8fee1c5384fa63780f94bdd880dcda4e7b9
DongChyeon <kimdonghyun90766@gmail.com>
2023-08-10 13:54:56 +0900

    WIN-58 Feat : FakeLoginViewModel 작성 실제 비즈니스 로직을 담은 뷰모델은 아니고 상속받아서 해당 방식으로 사용할려고 의도했다는 것을 알리기 위해 작성했어요

---
+++ feature/login/src/main/java/com/teamwiney/login/LoginContract.kt
@@ -0,0 +1,26 @@
+package com.teamwiney.login
+
+import com.teamwiney.core.common.base.UiEffect
+import com.teamwiney.core.common.base.UiEvent
+import com.teamwiney.core.common.base.UiState
+
+class LoginContract {
+
+    data class State(
+        val isLoading: Boolean = false,
+        val lastLoginMethod: String? = null,
+        val error: String? = null
+    ) : UiState
+
+    sealed class Event : UiEvent {
+        object KaKaoLoginButtonClicked : Event()
+        object NaverLoginButtonClicked : Event()
+        object GoogleLoginButtonClicked : Event()
+    }
+
+    sealed class Effect : UiEffect {
+        object NavigateToHome : Effect()
+        data class ShowSnackbar(val message: String) : Effect()
+    }
+
+}
