5d462a8ff755330b56a04935e23a5e7718987734
DongChyeon <kimdonghyun90766@gmail.com>
2023-08-11 22:16:42 +0900

    Feat : feature 모듈 간 네비게이션 이동을 app 모듈이 중재하도록 변경

--- app/src/main/java/com/teamwiney/winey/WineyNavHost.kt
+++ app/src/main/java/com/teamwiney/winey/WineyNavHost.kt
@@ -1,8 +1,10 @@
 package com.teamwiney.winey
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
 import com.teamwiney.login.loginGraph
 import com.teamwiney.login.navigateToLoginGraph
 import com.teamwiney.signup.navigateToSignUpGraph
@@ -18,8 +20,21 @@ fun WineyNavHost() {
         navController = navController,
         startDestination = SPLASH
     ) {
+        // feature 모듈 별로 네비게이션 로직이 존재하는데도 불구하고 네비게이션 경로를 담은 전역 상수 파일은 무의미하다고 생각됨
+        // feature 모듈이 모둡 바인딩된 app 모듈에서 람다식으로 모듈간 경로 이동을 처리해주는 게 맞는다고 생각함
+        // [변경 근거 : 모듈 간 통신 시 중재 모듈을 app 모듈로 설정] https://developer.android.com/topic/modularization/patterns?hl=ko
+        // TODO : 네비게이션 로직 관리에 대한 더 좋은 아이디어가 떠오르면 바꿀 예정
         splashComposable(
-            navController = navController,
+            onCompleted = {
+                navController.navigateToLoginGraph(
+                    navOptions = navOptions {
+                        NavOptionsBuilder().popUpTo(
+                            route = SPLASH,
+                            popUpToBuilder = { inclusive = true }
+                        )
+                    }
+                )
+            }
         )
         loginGraph(
             navigateToJoinGraph = { navController.navigateToSignUpGraph() }
--- feature/login/src/main/java/com/teamwiney/splash/SplashNavigation.kt
+++ feature/login/src/main/java/com/teamwiney/splash/SplashNavigation.kt
@@ -1,20 +1,19 @@
 package com.teamwiney.splash
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.teamwiney.graphs.LOGIN_ROUTE
 
 const val SPLASH = "splash"
 
 fun NavGraphBuilder.splashComposable(
-    navController: NavController
+    onCompleted : () -> Unit
 ) {
     composable(route = SPLASH) {
         SplashScreen(onCompleted = {
-            navController.navigate(LOGIN_ROUTE) {
-                popUpTo(SPLASH) { inclusive = true }
-            }
+            onCompleted()
+            //navController.navigate(LOGIN_ROUTE) {
+            //    popUpTo(SPLASH) { inclusive = true }
+            //}
         })
     }
 }
