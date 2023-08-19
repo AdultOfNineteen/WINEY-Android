027c2e4c9f6841a39a552c275347516a54a39e26
DongChyeon <kimdonghyun90766@gmail.com>
2023-08-19 16:12:59 +0900

    WIN-65 Design : Winecard 컴포넌트 기초 작업 완료

--- core/design/src/main/java/com/teamwiney/ui/components/WIneCard.kt
+++ core/design/src/main/java/com/teamwiney/ui/components/WIneCard.kt
@@ -1,44 +1,273 @@
 package com.teamwiney.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.geometry.toRect
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathOperation
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.teamwiney.core.design.R
 import com.teamwiney.ui.theme.WineyTheme
 
 @Composable
 fun WineCard(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    color: String,
+    name: String,
+    origin: String,
+    price: String
 ) {
     Surface(
-        modifier = modifier,
+        modifier = modifier
+            .width(IntrinsicSize.Min)
+            .height(IntrinsicSize.Min),
         color = WineyTheme.colors.gray_50.copy(alpha = 0.3f),
         shape = TicketShape(
-            circleRadius = 50.dp,
+            circleRadius = 38.dp,
             circleOffsetY = 20.dp,
             cornerSize = CornerSize(4.dp)
         )
     ) {
-        // TODO : 컨텐츠 채우기
+        // 여기는 그냥 배경 원 그라데이션 코드상으로 넣은거예요
+        // 이미지 받으면은 이미지 Box 내에 넣고 Box 블러 처리하면 될 거 같아요
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .blur(50.dp)
+        ) {
+            BlurredCircle(
+                modifier = Modifier
+                    .padding(start = 23.dp, top = 63.dp)
+                    .size(157.dp),
+                colors = listOf(Color(0xFF6F0303), Color(0xFF6F036400))
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .blur(50.dp),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            BlurredCircle(
+                modifier = Modifier
+                    .padding(end = 20.dp, bottom = 48.dp)
+                    .size(178.dp),
+                colors = listOf(Color(0xFF6F0303), Color(0xFF6F036400))
+            )
+        }
+
+        WineCardContent(
+            color = color,
+            name = name,
+            origin = origin,
+            price = price
+        )
     }
+}
+
+@Composable
+private fun WineCardContent(
+    color: String,
+    name: String,
+    origin: String,
+    price: String
+) {
+    val dividerColorSteps = arrayOf(
+        0.0f to WineyTheme.colors.gray_50,
+        0.7f to WineyTheme.colors.gray_400,
+        1.0f to WineyTheme.colors.gray_700
+    )
+
+    Column(
+        modifier = Modifier
+            .width(IntrinsicSize.Max)
+            .padding(30.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Text(
+                text = color,
+                style = TextStyle(
+                    fontSize = 54.sp,
+                    fontFamily = FontFamily(Font(R.font.chaviera)),
+                    fontWeight = FontWeight(400),
+                    color = WineyTheme.colors.gray_50,
+                    textAlign = TextAlign.Center,
+                )
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.ic_thismooth),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+        }
+        Text(
+            text = name,
+            style = WineyTheme.typography.bodyM2,
+            color = WineyTheme.colors.gray_50
+        )
+        HeightSpacer(height = 10.dp)
+
+        HorizontalGradientDivider(colorStops = dividerColorSteps)
+
+        Row(modifier = Modifier.height(IntrinsicSize.Min)) {
+            Box(
+                modifier = Modifier.fillMaxHeight(),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = null
+                )
+            }
+
+            VerticalGradientDivider(colorStops = dividerColorSteps)
+
+            Column {
+                Column(modifier = Modifier.padding(10.dp)) {
+                    Text(
+                        text = "national anthems",
+                        style = TextStyle(
+                            fontSize = 8.sp,
+                            fontFamily = FontFamily(Font(R.font.chaviera)),
+                            fontWeight = FontWeight(400),
+                            color = WineyTheme.colors.gray_50,
+                        )
+                    )
+                    HeightSpacer(height = 5.dp)
+                    Text(
+                        modifier = Modifier,
+                        text = origin,
+                        style = WineyTheme.typography.bodyB1,
+                        color = WineyTheme.colors.gray_50
+                    )
+                }
+
+                HorizontalGradientDivider(colorStops = dividerColorSteps)
+
+                Column(modifier = Modifier.padding(10.dp)) {
+                    Text(
+                        text = "Varities",
+                        style = TextStyle(
+                            fontSize = 8.sp,
+                            fontFamily = FontFamily(Font(R.font.chaviera)),
+                            fontWeight = FontWeight(400),
+                            color = WineyTheme.colors.gray_50,
+                        )
+                    )
+                    HeightSpacer(height = 5.dp)
+                    Text(
+                        modifier = Modifier,
+                        text = "프리미티보",
+                        style = WineyTheme.typography.bodyB1,
+                        color = WineyTheme.colors.gray_50
+                    )
+                }
+
+                HorizontalGradientDivider(colorStops = dividerColorSteps)
+
+                Column(modifier = Modifier.padding(10.dp)) {
+                    Text(
+                        text = "Purchase Price",
+                        style = TextStyle(
+                            fontSize = 8.sp,
+                            fontFamily = FontFamily(Font(R.font.chaviera)),
+                            fontWeight = FontWeight(400),
+                            color = WineyTheme.colors.gray_50,
+                        )
+                    )
+                    HeightSpacer(height = 5.dp)
+                    Text(
+                        modifier = Modifier,
+                        text = price,
+                        style = WineyTheme.typography.bodyB1,
+                        color = WineyTheme.colors.gray_50
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun BlurredCircle(
+    modifier: Modifier = Modifier,
+    colors: List<Color>
+) {
+    Spacer(
+        modifier = modifier
+            .background(
+                brush = Brush.verticalGradient(colors),
+                shape = CircleShape
+            )
+    )
+}
+
+@Composable
+private fun HorizontalGradientDivider(
+    colorStops: Array<Pair<Float, Color>>
+) {
+    Spacer(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(Brush.horizontalGradient(colorStops = colorStops))
+    )
+}
+
+@Composable
+private fun VerticalGradientDivider(
+    colorStops: Array<Pair<Float, Color>>
+) {
+    Spacer(
+        modifier = Modifier
+            .width(1.dp)
+            .fillMaxHeight()
+            .background(Brush.verticalGradient(colorStops = colorStops))
+    )
 }
 
 class TicketShape(
@@ -96,7 +325,12 @@ fun PreviewWineCard() {
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            WineCard(modifier = Modifier.size(400.dp))
+            WineCard(
+                color = "RED",
+                name = "캄포 마리나 프리미티도 디 만두리아",
+                origin = "이탈리아",
+                price = "8.80"
+            )
         }
     }
 }
