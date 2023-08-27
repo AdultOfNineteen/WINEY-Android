8980998b0cccc1770b83d77500de4a1eccd783c8
PCBL <cksgodl1@naver.com>
2023-08-27 17:43:13 +0900

    Design : 로고 및 앱이름 파스칼케이스로 변경

--- feature/auth/src/main/java/com/teamwiney/auth/login/LoginScreen.kt
+++ feature/auth/src/main/java/com/teamwiney/auth/login/LoginScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
@@ -73,6 +74,7 @@ fun LoginScreen(
         LoginTitle()
         Column(
             modifier = Modifier
+                .navigationBarsPadding()
                 .align(Alignment.BottomCenter)
                 .fillMaxWidth()
                 .padding(bottom = 53.dp),
