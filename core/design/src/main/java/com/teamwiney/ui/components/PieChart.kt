d715fbf9cf0a1ba020ed3fc543feed4c3a580854
DongChyeon <kimdonghyun90766@gmail.com>
2023-09-14 09:21:55 +0900

    WIN-65 Design : PieChart 컴포넌트 라벨 위치 조정

--- core/design/src/main/java/com/teamwiney/ui/components/PieChart.kt
+++ core/design/src/main/java/com/teamwiney/ui/components/PieChart.kt
@@ -3,8 +3,9 @@ package com.teamwiney.ui.components
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -18,46 +19,47 @@ import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.center
-import androidx.compose.ui.unit.dp
 import com.teamwiney.ui.theme.WineyTheme
 import kotlin.math.cos
 import kotlin.math.sin
 
-@Preview
+data class ChartData(
+    val label: String,
+    val color: Color,
+    val value: Float,
+    val textStyle: TextStyle
+)
+
 @Composable
 fun PieChart(
-    chartDataList: List<ChartData> = listOf(
-        ChartData("레드", Color(0xFF5123DF), 75f),
-        ChartData("화이트", Color(0xFF5E489B), 18f),
-        ChartData("로제", WineyTheme.colors.gray_800, 7f)
-    ),
-    textStyle: TextStyle = WineyTheme.typography.title2
+    modifier: Modifier = Modifier,
+    chartDataList: List<ChartData>
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(WineyTheme.colors.background_1),
-        contentAlignment = Alignment.Center
-    ) {
-        val textMeasurer = rememberTextMeasurer()
-        val textMeasureResults = remember(chartDataList) {
-            chartDataList.mapIndexed { index, chartData ->
-                val labelText = if (index == 0) {
-                    "${chartData.label}\n${chartData.data}%"
-                } else {
-                    chartData.label
-                }
-                textMeasurer.measure(
-                    text = labelText,
-                    style = textStyle
-                )
-            }
-        }
 
-        Canvas(
-            modifier = Modifier.size(280.dp)
-        ) {
-            val width = size.width
+    val textMeasurer = rememberTextMeasurer()
+    val textMeasureResults = remember(chartDataList) {
+        chartDataList.mapIndexed { index, chartData ->
+            val labelText = if (index == 0) {
+                "${chartData.label}\n${chartData.value}%"
+            } else {
+                chartData.label
+            }
+            textMeasurer.measure(
+                text = labelText,
+                style = chartData.textStyle
+            )
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+    ) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val width = size.width / 2
+            val height = size.height / 2
+
             val radius = width / 3f
             val strokeWidth = radius * .4f
             val gapAngle = 2f // Arc 사이의 간격 각도
@@ -67,7 +69,7 @@ fun PieChart(
             for (index in 0..chartDataList.lastIndex) {
 
                 val chartData = chartDataList[index]
-                val sweepAngle = chartData.data.asAngle
+                val sweepAngle = chartData.value.asAngle
                 val angleInRadians = (startAngle + sweepAngle / 2).degreeToAngle
                 val textMeasureResult = textMeasureResults[index]
                 val textSize = textMeasureResult.size
@@ -79,7 +81,7 @@ fun PieChart(
                         startAngle = startAngle,
                         sweepAngle = sweepAngle - gapAngle,
                         useCenter = false,
-                        topLeft = Offset(strokeWidth / 2, strokeWidth / 2),
+                        topLeft = Offset(width / 1.75f, height / 1.75f),
                         size = Size(width - strokeWidth, width - strokeWidth),
                         style = Stroke(strokeWidth * 1.2f) // 두께를 조절합니다.
                     )
@@ -89,7 +91,7 @@ fun PieChart(
                         startAngle = startAngle,
                         sweepAngle = sweepAngle - gapAngle,
                         useCenter = false,
-                        topLeft = Offset(strokeWidth / 2, strokeWidth / 2),
+                        topLeft = Offset(width / 1.75f, height / 1.75f),
                         size = Size(width - strokeWidth, width - strokeWidth),
                         style = Stroke(strokeWidth)
                     )
@@ -97,19 +99,25 @@ fun PieChart(
 
                 val textCenter = textSize.center
 
-                val legendRadius = width / 2f + textCenter.x + 60f
-                val legendX = center.x + legendRadius * cos(angleInRadians)
+                val legendRadius = if (startAngle >= 235f) {
+                    width / 2f + textSize.width
+                } else if (startAngle >= 180f && startAngle + sweepAngle >= 235f) {
+                    width / 2f + textSize.width + 20f
+                } else {
+                    width / 2f + textCenter.x + 30f
+                }
+
+                val legendX = center.x + legendRadius * cos(angleInRadians) + 65f
                 val legendY = center.y + legendRadius * sin(angleInRadians)
 
                 drawCircle(
                     color = chartData.color,
-                    radius = 10f, // 점의 반지름을 조절합니다.
-                    center = Offset(legendX - textSize.width / 1.5f, legendY - textSize.height / 4f)
+                    radius = 10f,
+                    center = Offset(legendX - textCenter.x - 20f, legendY - textCenter.y + 10f)
                 )
 
                 drawText(
                     textLayoutResult = textMeasureResult,
-                    color = Color.Gray,
                     topLeft = Offset(
                         legendX - textCenter.x,
                         legendY - textCenter.y
@@ -128,4 +136,42 @@ private val Float.degreeToAngle
 private val Float.asAngle: Float
     get() = this * 360f / 100f
 
-data class ChartData(val label: String, val color: Color, val data: Float)
+@Preview
+@Composable
+fun PreviewPieChart() {
+    WineyTheme {
+        Box(
+            modifier = Modifier.fillMaxSize().background(WineyTheme.colors.background_1),
+            contentAlignment = Alignment.Center
+        ) {
+            PieChart(
+                chartDataList = listOf(
+                    ChartData(
+                        label = "레드",
+                        color = Color(0xFF5123DF),
+                        value = 75f,
+                        textStyle = WineyTheme.typography.title2.copy(
+                            color = WineyTheme.colors.gray_50
+                        )
+                    ),
+                    ChartData(
+                        label = "화이트",
+                        color = Color(0xFF5E489B),
+                        value = 18f,
+                        textStyle = WineyTheme.typography.bodyB2.copy(
+                            color = WineyTheme.colors.gray_800
+                        )
+                    ),
+                    ChartData(
+                        label = "로제",
+                        color = WineyTheme.colors.gray_800,
+                        value = 7f,
+                        textStyle = WineyTheme.typography.captionB1.copy(
+                            color = WineyTheme.colors.gray_900
+                        )
+                    )
+                )
+            )
+        }
+    }
+}
