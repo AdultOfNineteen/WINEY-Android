18a219b3707f16343d732f10987ba2b343331bf1
DongChyeon <kimdonghyun90766@gmail.com>
2023-11-04 00:05:30 +0900

    WIN-109 Design : NumberPicker 컴포넌트 작성 (애니메이션 부드럽게 개선 필요)

---
+++ core/design/src/main/java/com/teamwiney/ui/components/NumberPicker.kt
@@ -0,0 +1,282 @@
+package com.teamwiney.ui.components
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationResult
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.DecayAnimationSpec
+import androidx.compose.animation.core.calculateTargetValue
+import androidx.compose.animation.core.exponentialDecay
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.ProvideTextStyle
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.teamwiney.ui.theme.WineyTheme
+import kotlinx.coroutines.launch
+import kotlin.math.abs
+import kotlin.math.roundToInt
+
+private fun <T> getItemIndexForOffset(
+    range: List<T>,
+    value: T,
+    offset: Float,
+    halfNumbersColumnHeightPx: Float
+): Int {
+    val indexOf = range.indexOf(value) - (offset / halfNumbersColumnHeightPx).toInt()
+    return maxOf(0, minOf(indexOf, range.count() - 1))
+}
+
+@Composable
+fun <T> ListItemPicker(
+    modifier: Modifier = Modifier,
+    label: (T) -> String = { it.toString() },
+    value: T,
+    onValueChange: (T) -> Unit,
+    list: List<T>,
+    textStyle: TextStyle = LocalTextStyle.current,
+) {
+    val minimumAlpha = 0.3f
+    val numbersColumnHeight = 80.dp
+    val halfNumbersColumnHeight = numbersColumnHeight / 2
+    val halfNumbersColumnHeightPx = with(LocalDensity.current) { halfNumbersColumnHeight.toPx() }
+
+    val coroutineScope = rememberCoroutineScope()
+
+    val animatedOffset = remember { Animatable(0f) }
+        .apply {
+            val index = list.indexOf(value)
+            val offsetRange = remember(value, list) {
+                -((list.count() - 1) - index) * halfNumbersColumnHeightPx to
+                        index * halfNumbersColumnHeightPx
+            }
+            updateBounds(offsetRange.first, offsetRange.second)
+        }
+
+    val coercedAnimatedOffset = animatedOffset.value % halfNumbersColumnHeightPx
+
+    val indexOfElement = getItemIndexForOffset(list, value, animatedOffset.value, halfNumbersColumnHeightPx)
+
+    var dividersWidth by remember { mutableStateOf(0.dp) }
+
+    Layout(
+        modifier = modifier
+            .draggable(
+                orientation = Orientation.Vertical,
+                state = rememberDraggableState { deltaY ->
+                    coroutineScope.launch {
+                        animatedOffset.snapTo(animatedOffset.value + deltaY)
+                    }
+                },
+                onDragStopped = { velocity ->
+                    coroutineScope.launch {
+                        val endValue = animatedOffset.fling(
+                            initialVelocity = velocity,
+                            animationSpec = exponentialDecay(frictionMultiplier = 20f),
+                            adjustTarget = { target ->
+                                val coercedTarget = target % halfNumbersColumnHeightPx
+                                val coercedAnchors =
+                                    listOf(
+                                        -halfNumbersColumnHeightPx,
+                                        0f,
+                                        halfNumbersColumnHeightPx
+                                    )
+                                val coercedPoint =
+                                    coercedAnchors.minByOrNull { abs(it - coercedTarget) }!!
+                                val base =
+                                    halfNumbersColumnHeightPx * (target / halfNumbersColumnHeightPx).toInt()
+                                coercedPoint + base
+                            }
+                        ).endState.value
+
+                        val result = list.elementAt(
+                            getItemIndexForOffset(list, value, endValue, halfNumbersColumnHeightPx)
+                        )
+                        onValueChange(result)
+                        animatedOffset.snapTo(0f)
+                    }
+                }
+            )
+            .padding(vertical = numbersColumnHeight / 3),
+        content = {
+            Box(
+                modifier
+                    .width(dividersWidth)
+            )
+            Box(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .offset { IntOffset(x = 0, y = coercedAnimatedOffset.roundToInt()) }
+            ) {
+                val baseLabelModifier = Modifier.align(Alignment.Center)
+                ProvideTextStyle(textStyle) {
+                    if (indexOfElement > 1)
+                        Label(
+                            text = label(list.elementAt(indexOfElement - 2)),
+                            modifier = baseLabelModifier
+                                .offset(y = -halfNumbersColumnHeight * 0.625f - 16.dp)
+                                .alpha(0.3f)
+                                .scale(scaleX = 0.65f, scaleY = 0.25f)
+                        )
+                    if (indexOfElement > 0)
+                        Label(
+                            text = label(list.elementAt(indexOfElement - 1)),
+                            modifier = baseLabelModifier
+                                .offset(y = -halfNumbersColumnHeight * 0.375f - 9.dp)
+                                .alpha(0.5f)
+                                .scale(scaleX = 0.75f, scaleY = 0.42f)
+                        )
+                    Label(
+                        text = label(list.elementAt(indexOfElement)),
+                        modifier = baseLabelModifier
+                            .alpha(
+                                (maxOf(
+                                    minimumAlpha,
+                                    1 - abs(coercedAnimatedOffset) / halfNumbersColumnHeightPx
+                                ))
+                            )
+                    )
+                    if (indexOfElement < list.count() - 1)
+                        Label(
+                            text = label(list.elementAt(indexOfElement + 1)),
+                            modifier = baseLabelModifier
+                                .offset(y = halfNumbersColumnHeight * 0.375f + 9.dp)
+                                .alpha(0.5f)
+                                .scale(scaleX = 0.75f, scaleY = 0.42f)
+                        )
+                    if (indexOfElement < list.count() - 2)
+                        Label(
+                            text = label(list.elementAt(indexOfElement + 2)),
+                            modifier = baseLabelModifier
+                                .offset(y = halfNumbersColumnHeight * 0.625f + 16.dp)
+                                .alpha(0.3f)
+                                .scale(scaleX = 0.65f, scaleY = 0.25f)
+                        )
+                }
+            }
+        }
+    ) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        dividersWidth = placeables
+            .drop(1)
+            .first()
+            .width
+            .toDp()
+
+        layout(dividersWidth.toPx().toInt(), placeables
+            .sumOf {
+                it.height
+            }
+        ) {
+            var yPosition = 0
+
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+private fun Label(text: String, modifier: Modifier) {
+    Text(
+        modifier = modifier,
+        text = text,
+        textAlign = TextAlign.Center,
+    )
+}
+
+private suspend fun Animatable<Float, AnimationVector1D>.fling(
+    initialVelocity: Float,
+    animationSpec: DecayAnimationSpec<Float>,
+    adjustTarget: ((Float) -> Float)?,
+    block: (Animatable<Float, AnimationVector1D>.() -> Unit)? = null,
+): AnimationResult<Float, AnimationVector1D> {
+    val targetValue = animationSpec.calculateTargetValue(value, initialVelocity)
+    val adjustedTarget = adjustTarget?.invoke(targetValue)
+    return if (adjustedTarget != null) {
+        animateTo(
+            targetValue = adjustedTarget,
+            initialVelocity = initialVelocity,
+            block = block
+        )
+    } else {
+        animateDecay(
+            initialVelocity = initialVelocity,
+            animationSpec = animationSpec,
+            block = block,
+        )
+    }
+}
+
+@Composable
+fun NumberPicker(
+    modifier: Modifier = Modifier,
+    label: (Int) -> String = {
+        it.toString()
+    },
+    value: Int,
+    onValueChange: (Int) -> Unit,
+    range: Iterable<Int>,
+    textStyle: TextStyle = LocalTextStyle.current
+) {
+    ListItemPicker(
+        modifier = modifier,
+        label = label,
+        value = value,
+        onValueChange = onValueChange,
+        list = range.toList(),
+        textStyle = textStyle
+    )
+}
+
+@Preview
+@Composable
+fun PreviewNumberPicker() {
+    WineyTheme {
+        var value by remember { mutableStateOf(20) }
+
+        Box(
+            modifier = Modifier.fillMaxSize().background(WineyTheme.colors.background_1),
+            contentAlignment = Alignment.Center
+        ) {
+            NumberPicker(
+                value = value,
+                onValueChange = {
+                    value = it
+                },
+                range = 0..40,
+                textStyle = WineyTheme.typography.title1.copy(
+                    color = WineyTheme.colors.gray_50
+                )
+            )
+        }
+    }
+}
