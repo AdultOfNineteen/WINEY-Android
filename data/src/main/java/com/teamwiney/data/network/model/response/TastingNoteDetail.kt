62b7bebaedd43d54a973d6264ecc89e3775713db
DongChyeon <kimdonghyun90766@gmail.com>
2023-11-04 22:45:31 +0900

    Fix : TastingNoteDetail 맛 정보 Double -> Int 타입으로 변경

--- data/src/main/java/com/teamwiney/data/network/model/response/TastingNoteDetail.kt
+++ data/src/main/java/com/teamwiney/data/network/model/response/TastingNoteDetail.kt
@@ -10,7 +10,7 @@ data class TastingNoteDetail(
     val color: String,
     val buyAgain: Boolean,
     val varietal: String,
-    val officialAlcohol: Double,
+    val officialAlcohol: Int,
     val price: Int,
     val smellKeywordList: List<String>,
     val myWineTaste: MyWineTaste,
@@ -29,62 +29,42 @@ data class TastingNoteDetail(
             color = "RED",
             buyAgain = true,
             varietal = "varietal",
-            officialAlcohol = 0.0,
+            officialAlcohol = 0,
             price = 0,
             smellKeywordList = listOf("-"),
             myWineTaste = MyWineTaste(
-                sweetness = 0.0,
-                acidity = 0.0,
-                tannin = 0.0,
-                body = 0.0,
-                alcohol = 0.0,
-                finish = 0.0
+                sweetness = 0,
+                acidity = 0,
+                tannin = 0,
+                body = 0,
+                alcohol = 0,
+                finish = 0
             ),
             defaultWineTaste = WineTaste(
-                sweetness = 0.0,
-                acidity = 0.0,
-                tannin = 0.0,
-                body = 0.0,
+                sweetness = 0,
+                acidity = 0,
+                tannin = 0,
+                body = 0,
             ),
             memo = "-",
             tastingNoteImage = listOf(TastingNoteImage("1", "1")),
         )
     }
-
-    fun getWine() = Wine(
-        wineId = -1,
-        type = this.wineType,
-        name = this.wineName,
-        country = this.region,
-        varietal = this.varietal,
-        sweetness = this.myWineTaste.sweetness.toInt(),
-        acidity = this.myWineTaste.acidity.toInt(),
-        body = this.myWineTaste.body.toInt(),
-        tannins = this.myWineTaste.tannin.toInt(),
-        wineSummary = WineSummary(
-            avgPrice = this.price.toDouble(),
-            avgSweetness = this.defaultWineTaste.sweetness.toInt(),
-            avgAcidity = this.defaultWineTaste.acidity.toInt(),
-            avgBody = this.defaultWineTaste.body.toInt(),
-            avgTannins = this.defaultWineTaste.tannin.toInt()
-        )
-    )
 }
-
 data class WineTaste(
-    val sweetness: Double,
-    val acidity: Double,
-    val tannin: Double,
-    val body: Double,
+    val sweetness: Int,
+    val acidity: Int,
+    val tannin: Int,
+    val body: Int,
 )
 
 data class MyWineTaste(
-    val sweetness: Double,
-    val acidity: Double,
-    val tannin: Double,
-    val body: Double,
-    val alcohol: Double,
-    val finish: Double
+    val sweetness: Int,
+    val acidity: Int,
+    val tannin: Int,
+    val body: Int,
+    val alcohol: Int,
+    val finish: Int
 )
 
 data class TastingNoteImage(
