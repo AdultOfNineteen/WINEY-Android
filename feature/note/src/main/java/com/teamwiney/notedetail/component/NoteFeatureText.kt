def674bffc98735fc145ff9fdc010d0385ef7a57
PCBL <cksgodl1@naver.com>
2023-11-17 01:37:04 +0900

    WIN-109 Design : 와인 메모 화면 작성 완료
    동천의 커스텀 갤러리 믿습니다.

--- feature/note/src/main/java/com/teamwiney/notedetail/component/NoteFeatureText.kt
+++ feature/note/src/main/java/com/teamwiney/notedetail/component/NoteFeatureText.kt
@@ -1,33 +1,45 @@
 package com.teamwiney.notedetail.component
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.teamwiney.ui.theme.WineyTheme
 
 
 @Composable
+@Preview
 fun NoteFeatureText(
-    name: String,
+    name: String = "테스트",
+    enable: Boolean = false,
+    onClick: () -> Unit = {}
 ) {
     Text(
         text = name,
-        color = WineyTheme.colors.gray_700,
+        color = if (enable) WineyTheme.colors.gray_50 else WineyTheme.colors.gray_700,
         style = WineyTheme.typography.captionB1,
         modifier = Modifier
             .clip(RoundedCornerShape(40.dp))
             .border(
                 BorderStroke(
-                    1.dp, WineyTheme.colors.gray_700
+                    1.dp, if (enable) WineyTheme.colors.main_2 else WineyTheme.colors.gray_700
                 ),
                 RoundedCornerShape(40.dp)
             )
+            .background(if (enable) WineyTheme.colors.main_2 else Color.Transparent)
+            .clickable {
+                onClick()
+            }
             .padding(10.dp)
+
     )
 }
--- feature/note/src/main/java/com/teamwiney/notewrite/NoteWineInfoMemoScreen.kt
+++ feature/note/src/main/java/com/teamwiney/notewrite/NoteWineInfoMemoScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,21 +18,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -43,14 +44,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -59,13 +57,10 @@ import com.bumptech.glide.integration.compose.GlideImage
 import com.teamwiney.core.common.WineyAppState
 import com.teamwiney.core.common.rememberWineyAppState
 import com.teamwiney.core.design.R
-import com.teamwiney.notewrite.components.NoteBackgroundSurface
-import com.teamwiney.ui.components.HeightSpacer
-import com.teamwiney.ui.components.HeightSpacerWithLine
-import com.teamwiney.ui.components.NumberPicker
+import com.teamwiney.notedetail.component.NoteFeatureText
 import com.teamwiney.ui.components.TopBar
 import com.teamwiney.ui.components.WButton
-import com.teamwiney.ui.components.WTextField
+import com.teamwiney.ui.theme.Pretendard
 import com.teamwiney.ui.theme.WineyTheme
 
 @Composable
@@ -74,9 +69,10 @@ fun NoteWineInfoMemoScreen(
     viewModel: NoteWriteViewModel = hiltViewModel(),
 ) {
 
-    var vintage by remember { mutableStateOf("") }
+    var wineMemo by remember { mutableStateOf("") }
     val selectedImage = remember { mutableStateListOf<Uri?>() }
-
+    var repurchase by remember { mutableStateOf<Boolean?>(null) }
+    var score by remember { mutableIntStateOf(0) }
     val imagePicker = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetMultipleContents(),
         onResult = { uris ->
@@ -133,31 +129,69 @@ fun NoteWineInfoMemoScreen(
 
             Row(
                 horizontalArrangement = Arrangement.spacedBy(10.dp),
-                modifier = Modifier.padding(vertical = 15.dp)
+                modifier = Modifier
+                    .padding(vertical = 15.dp)
+                    .horizontalScroll(rememberScrollState())
             ) {
                 selectedImage.map {
-                    GlideImage(
-                        model = it,
-                        contentDescription = "IMG_URL",
+                    Box {
+                        GlideImage(
+                            model = it,
+                            contentDescription = "IMG_URL",
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(WineyTheme.colors.gray_900),
+                            contentScale = ContentScale.Crop
+                        )
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_close_fill_18),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .padding(7.dp)
+                                .align(Alignment.TopEnd)
+                                .clickable {
+                                    selectedImage.remove(it)
+                                }
+                                .size(18.dp),
+                            tint = Color.Unspecified
+                        )
+                    }
+                }
+            }
+
+            Button(
+                onClick = {
+                    imagePicker.launch("image/*")
+                },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                border = BorderStroke(1.dp, WineyTheme.colors.main_2),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                ),
+                shape = RoundedCornerShape(5.dp),
+                enabled = true
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "사진 첨부하기",
+                        color = WineyTheme.colors.main_2,
+                        style = WineyTheme.typography.bodyM2,
+                        modifier = Modifier.padding(vertical = 8.5.dp)
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_camera_baseline_30),
+                        contentDescription = null,
                         modifier = Modifier
-                            .size(100.dp)
-                            .clip(RoundedCornerShape(10.dp))
-                            .background(WineyTheme.colors.gray_900),
-                        contentScale = ContentScale.Crop
+                            .size(30.dp)
                     )
                 }
             }
 
-            WButton(
-                text = "사진 첨부하기",
-                modifier = Modifier
-                    .fillMaxWidth(),
-                enableBackgroundColor = Color.Transparent,
-                border = BorderStroke(1.dp, WineyTheme.colors.main_2),
-                onClick = {
-                    imagePicker.launch("image/*")
-                }
-            )
 
             Box(
                 modifier = Modifier
@@ -173,13 +207,32 @@ fun NoteWineInfoMemoScreen(
                     )
             ) {
                 BasicTextField(
-                    value = vintage,
-                    onValueChange = { vintage = it },
+                    value = wineMemo,
+                    onValueChange = { wineMemo = it },
                     modifier = Modifier
                         .padding(14.dp)
                         .fillMaxWidth()
                         .heightIn(min = 150.dp),
-                    textStyle = WineyTheme.typography.captionM1,
+                    textStyle = WineyTheme.typography.captionM1.copy(
+                        color = WineyTheme.colors.gray_50
+                    ),
+                    decorationBox = { innerTextField ->
+                        Box {
+                            if (wineMemo.isEmpty()) {
+                                androidx.compose.material3.Text(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    text = "와인에 대한 생각을 작성해주세요 :)",
+                                    style = LocalTextStyle.current.copy(
+                                        color = WineyTheme.colors.gray_800,
+                                        fontSize = 13.sp,
+                                        fontFamily = Pretendard
+                                    ),
+                                    textAlign = TextAlign.Start,
+                                )
+                            }
+                            innerTextField()
+                        }
+                    },
                 )
             }
             Text(
@@ -202,17 +255,23 @@ fun NoteWineInfoMemoScreen(
                     style = WineyTheme.typography.bodyB1,
                     color = WineyTheme.colors.gray_50,
                 )
-                Row {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(5.dp)
+                ) {
                     repeat(5) {
                         Icon(
-                            painter = painterResource(id = R.drawable.ic_etc_wine_badge),
+                            painter = painterResource(id = if (score > it) R.drawable.ic_wine_fill_30 else R.drawable.ic_wine_unfill_30),
                             contentDescription = null,
-                            modifier = Modifier.size(30.dp)
+                            modifier = Modifier
+                                .size(30.dp)
+                                .clickable {
+                                    score = it + 1
+                                },
+                            tint = Color.Unspecified
                         )
                     }
                 }
             }
-
 
             Row(
                 modifier = Modifier
@@ -225,6 +284,16 @@ fun NoteWineInfoMemoScreen(
                     style = WineyTheme.typography.bodyB1,
                     color = WineyTheme.colors.gray_50,
                 )
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(6.dp)
+                ) {
+                    NoteFeatureText(name = "있어요", enable = repurchase == true) {
+                        repurchase = true
+                    }
+                    NoteFeatureText(name = "없어요", enable = repurchase == false) {
+                        repurchase = false
+                    }
+                }
             }
             Spacer(modifier = Modifier.weight(1f))
 
@@ -236,8 +305,9 @@ fun NoteWineInfoMemoScreen(
                 disableBackgroundColor = WineyTheme.colors.gray_900,
                 disableTextColor = WineyTheme.colors.gray_600,
                 enableTextColor = WineyTheme.colors.gray_50,
+                enabled = score != 0 && repurchase != null,
                 onClick = {
-
+                    // TODO 작성 완료 화면 이동
                 }
             )
         }
