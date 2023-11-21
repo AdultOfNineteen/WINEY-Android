6d8f1e8d002ca2ca9cbdd470109166b4b7460ae4
PCBL <cksgodl1@naver.com>
2023-11-22 01:53:38 +0900

    design : 컬러 슬라이더 그라디언트 오류 수정
    캔버스 내에서 Brush가 정상적으로 안 그려진다면 캔버스를 사용 안하면 되지요

--- core/design/src/main/java/com/teamwiney/ui/components/ColorSlider.kt
+++ core/design/src/main/java/com/teamwiney/ui/components/ColorSlider.kt
@@ -5,13 +5,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,9 +23,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.lerp
@@ -44,33 +50,42 @@ fun ColorSlider(
     var thumbX by remember { mutableStateOf(0f) }
     val isDragging by remember { mutableStateOf(true) }
 
-    Box {
+    Box(
+        modifier = Modifier.height(IntrinsicSize.Min)
+    ) {
+        Box(
+            modifier
+                .clip(RoundedCornerShape(100.dp))
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(startColor, endColor)
+                    )
+                )
+                .height(trackHeight)
+                .fillMaxWidth()
+        )
         Canvas(
-            modifier = modifier.fillMaxWidth()
+            modifier = modifier
+                .fillMaxWidth()
+                .align(Alignment.Center)
                 .pointerInput(true) {
                     detectHorizontalDragGestures { _, dragAmount ->
                         if (isDragging) {
                             thumbX += dragAmount
                             thumbX = thumbX.coerceIn(0f, size.width.toFloat())
-                            onValueChange(calculateColorForPosition(thumbX, size.width.toFloat(), startColor, endColor))
+                            onValueChange(
+                                calculateColorForPosition(
+                                    thumbX,
+                                    size.width.toFloat(),
+                                    startColor,
+                                    endColor
+                                )
+                            )
                         }
                     }
                 }
         ) {
-            val width = size.width
             val height = size.height
-
-            drawRoundRect(
-                brush = Brush.horizontalGradient(
-                    colors = listOf(startColor, endColor)
-                ),
-                topLeft = Offset(
-                    x = 0f,
-                    y = (height - trackHeight.toPx()) / 2
-                ),
-                size = Size(width = width, height = trackHeight.toPx()),
-                cornerRadius = CornerRadius(100.dp.toPx())
-            )
 
             drawCircle(
                 color = thumbColor,
@@ -88,7 +103,12 @@ private fun isInCircle(x: Float, y: Float, centerX: Float, centerY: Float, radiu
     return (dx * dx + dy * dy) <= (radius * radius)
 }
 
-private fun calculateColorForPosition(position: Float, width: Float, startColor: Color, endColor: Color): Color {
+private fun calculateColorForPosition(
+    position: Float,
+    width: Float,
+    startColor: Color,
+    endColor: Color
+): Color {
     val fraction = position / width
     return lerp(startColor, endColor, fraction)
 }
