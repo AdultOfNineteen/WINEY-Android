8982aefed1cdafb4c540ee84d85666d58547c676
DongChyeon <kimdonghyun90766@gmail.com>
2023-12-27 22:23:14 +0900

    Design : 노트 상세보기 바텀 시트 디자인 수정

--- feature/note/src/main/java/com/teamwiney/notedetail/component/NoteDetailBottomSheet.kt
+++ feature/note/src/main/java/com/teamwiney/notedetail/component/NoteDetailBottomSheet.kt
@@ -3,14 +3,18 @@ package com.teamwiney.notedetail.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.teamwiney.ui.components.HeightSpacer
 import com.teamwiney.ui.theme.WineyTheme
 
 
@@ -30,13 +34,21 @@ fun NoteDetailBottomSheet(
                 )
             )
             .padding(
-                start = 24.dp,
-                end = 24.dp,
                 top = 10.dp,
                 bottom = 20.dp
             ),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(
+            modifier = Modifier
+                .width(66.dp)
+                .height(5.dp)
+                .background(
+                    color = WineyTheme.colors.gray_900,
+                    shape = RoundedCornerShape(6.dp)
+                )
+        )
+        HeightSpacer(height = 10.dp)
         Text(
             text = "삭제하기",
             color = WineyTheme.colors.gray_50,
@@ -44,8 +56,12 @@ fun NoteDetailBottomSheet(
             modifier = Modifier
                 .fillMaxWidth()
                 .clickable { deleteNote() }
-                .padding(20.dp)
+                .padding(
+                    horizontal = 24.dp,
+                    vertical = 20.dp
+                )
         )
+        Spacer(modifier = Modifier.fillMaxWidth().background(WineyTheme.colors.gray_900))
         Text(
             text = "수정하기",
             color = WineyTheme.colors.gray_50,
@@ -55,7 +71,10 @@ fun NoteDetailBottomSheet(
                 .clickable {
                     patchNote()
                 }
-                .padding(20.dp)
+                .padding(
+                    horizontal = 24.dp,
+                    vertical = 20.dp
+                )
         )
     }
 }
