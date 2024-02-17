65d5c72de1c6742140208e7b06636556b9ebe741
PCBL <cksgodl1@naver.com>
2024-02-18 00:59:14 +0900

    refactor : 맵 관련 컴포넌트 분리

---
+++ feature/map/src/main/java/com/teamwiney/map/components/GpsIcon.kt
@@ -0,0 +1,59 @@
+package com.teamwiney.map.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.teamwiney.core.design.R
+import com.teamwiney.ui.theme.WineyTheme
+
+@Composable
+internal fun BoxScope.GpsIcon(
+    isCollapsed: Boolean,
+    peekHeight: Dp,
+    onClickGPSIcon: () -> Unit
+) {
+    AnimatedVisibility(
+        visible = isCollapsed,
+        modifier = Modifier.align(Alignment.BottomEnd),
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(
+                    end = 20.dp,
+                    bottom = peekHeight + 24.dp
+                )
+                .clip(CircleShape)
+                .background(WineyTheme.colors.gray_900)
+                .clickable {
+                    onClickGPSIcon()
+                }
+                .padding(13.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_gps_baseline_22),
+                contentDescription = "IC_GPS",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(22.dp),
+                tint = WineyTheme.colors.gray_50
+            )
+        }
+    }
+}
--- feature/map/src/main/java/com/teamwiney/map/manager/manageSystemUIColor.kt
+++ feature/map/src/main/java/com/teamwiney/map/manager/manageSystemUIColor.kt
@@ -5,10 +5,22 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.teamwiney.map.model.ShopCategory
 
 @Composable
-internal fun manageSystemUIColor() {
+internal fun manageSystemUIColor(selectedShopCategory: ShopCategory) {
     val systemUiController = rememberSystemUiController()
+    LaunchedEffect(key1 = selectedShopCategory) {
+        if (selectedShopCategory == ShopCategory.ALL) {
+            systemUiController.setSystemBarsColor(
+                color = Color.Transparent
+            )
+        } else {
+            systemUiController.setSystemBarsColor(
+                color = Color(0xB31F2126)
+            )
+        }
+    }
     DisposableEffect(key1 = Unit) {
         systemUiController.setSystemBarsColor(
             color = Color.Transparent
